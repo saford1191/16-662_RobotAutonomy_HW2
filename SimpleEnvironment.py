d9b0f4600d73ff4a63b7f733d6e08be0b9cbf6e4
saford1191 <saford1191@gmail.com>
2014-03-15 22:40:22 -0400

    Steve's SimpleEnvironment.py Update
    This is Steve's first crack at filling in the functions in SimpleEnvironment.py

--- SimpleEnvironment.py
+++ SimpleEnvironment.py
@@ -2,7 +2,6 @@ import numpy
 import pylab as pl
 import random
 import IPython
-import math
 
 class SimpleEnvironment(object):
 
@@ -25,49 +24,57 @@ class SimpleEnvironment(object):
         # goal sampling probability
         self.p = 0.0
 
-        # max movement from a known node to a new node
-        self.epsilon = 0.1 #meters
-
-
     def SetGoalParameters(self, goal_config, p = 0.2):
         self.goal_config = goal_config
         self.p = p
 
-
     def GenerateRandomConfiguration(self):
         lower_limits, upper_limits = self.boundary_limits
 
-        xPoint = random.uniform(lower_limits[0], upper_limits[0])
-        yPoint = random.uniform(lower_limits[1], upper_limits[1])
-
-        if (self.Collides([xPoint,yPoint])):
-            print("Collision detected")
+	found = False
+	while found == False:
+            xPoint = random.uniform(lower_limits[0], upper_limits[0])
+            yPoint = random.uniform(lower_limits[1], upper_limits[1])
+            if not self.Collides([xPoint,yPoint]):
+            	found = True
 
         print("x = "+str(xPoint) + " y = "+str(yPoint));
 
         config = [xPoint, yPoint]
-        # pl.plot(config, 'gx')
+        pl.plot(config, 'bx')
 
         return numpy.array(config)
 
-
     def ComputeDistance(self, start_config, end_config):
-        xdist = end_config[0] - start_config[0]
-        ydist = end_config[1] - start_config[1]
-        return math.sqrt(xdist * xdist + ydist * ydist)
-
+	x1 = start_config[0]
+	x2 = end_config[0]
+	y1 = start_config[1]
+	y2 = end_config[1]
+	return numpy.sqrt(((x2-x1)**2)+((y2-y1)**2))
 
     def Extend(self, start_config, end_config):
-        # TODO: Implement a function which attempts to extend from
-        #   a start configuration to a goal configuration
-        if self.ComputeDistance(start_config,end_config) < self.epsilon:
-            return end_config
-        else:
-            theta = math.atan2(end_config[1]-start_config[1],end_config[0]-start_config[0])
-            return start_config[0] + self.epsilon*math.cos(theta), start_config[1] + self.epsilon*math.sin(theta)
+	travel_limit = 0.01 # maximum distance allowed to travel towards goal
+	num_checks = 10 # number of collision checks along the way
+	dist = self.ComputeDistance(start_config, end_config)
 
-        pass
+	travel_dist = min(travel_limit, dist)
+	
+	x0 = start_config[0]
+	y0 = start_config[1]
+	x1 = end_config[0]
+	y1 = end_config[1]
+	xg = x0 + ( (travel_dist/dist)*(x1-x0) ) # xg and yg limited by travel_limit
+	yg = y0 + ( (travel_dist/dist)*(y1-y0) )
 
+	# incrementally step along line from start to goal, checking for collisions
+	for i in range(0, num_checks-1):
+	    config[0] = x0 + ( (i/(num_checks-1)) * (xg-x0) )
+	    config[1] = y0 + ( (i/(num_checks-1)) * (yg-y0) )
+	    if self.Collides(config):
+		return None
+	
+	return numpy.array([xg, yg])
+	
 
     def ShortenPath(self, path, timeout=5.0):
         #
@@ -77,68 +84,18 @@ class SimpleEnvironment(object):
         #
         return path
 
-
-    def Collides(self, point, theta):
-        # Show all obstacles in environment
-        for b in self.robot.GetEnv().GetBodies():
-            if b.GetName() == self.robot.GetName():
-                continue
-            bb = b.ComputeAABB()
-            tl = [bb.pos()[0] - bb.extents()[0], bb.pos()[1]-bb.extents()[1]]
-            br = [bb.pos()[0] + bb.extents()[0], bb.pos()[1]+bb.extents()[1]]
-
-            x = point[0]
-            y = point[1]
-
-            if ((x <= br[0]) and (x >= tl[0]) and (y <= tl[1]) and (y >= br[1])):
-                return True
-
-            return False
-
-
-    # Returns true if the point specified collides with an object in the space
     def Collides(self, point):
-        # Show all obstacles in environment
-        for b in self.robot.GetEnv().GetBodies():
-            if b.GetName() == self.robot.GetName():
-                continue
-            bb = b.ComputeAABB()
-            tl = [bb.pos()[0] - bb.extents()[0], bb.pos()[1]-bb.extents()[1]]
-            br = [bb.pos()[0] + bb.extents()[0], bb.pos()[1]+bb.extents()[1]]
+	#import IPython
+        #IPython.embed()
+	T = self.robot.GetTransform()
+	Tnew = T
+	Tnew[[0,1],3] = point
+	env = self.robot.GetEnv()
+	
+	# move robot to potential point, return collision between robot and table
+	self.robot.SetTransform(Tnew)
+	return env.CheckCollision(self.robot, env.GetKinBody('conference_table'))
 
-            x = point[0]
-            y = point[1]
-
-            if ((x <= br[0]) and (x >= tl[0]) and (y <= tl[1]) and (y >= br[1])):
-                return True
-
-        return False
-
-
-    # Returns true if the line segment  specified collides with an object in the space
-    def LineCollides(self, start_point, end_point):
-        # Show all obstacles in environment
-        for b in self.robot.GetEnv().GetBodies():
-            if b.GetName() == self.robot.GetName():
-                continue
-            bb = b.ComputeAABB()
-            tl = [bb.pos()[0] - bb.extents()[0], bb.pos()[1]-bb.extents()[1]]
-            br = [bb.pos()[0] + bb.extents()[0], bb.pos()[1]+bb.extents()[1]]
-
-            x = 0
-            y = 0
-
-            if ((x <= br[0]) and (x >= tl[0]) and (y <= tl[1]) and (y >= br[1])):
-                return True
-
-        return False
-
-    # returns true if the points p1 & p2 are within radius specified
-    def RadiusCollision(p1, p2, radius):
-        distance = self.ComputeDistance(p1,p2)
-        if (distance <= radius):
-            return True
-        return False
 
 
     def InitializePlot(self, goal_config):
@@ -164,9 +121,9 @@ class SimpleEnvironment(object):
                      bb.pos()[1] + bb.extents()[1],
                      bb.pos()[1] - bb.extents()[1]], 'r')
 
+
         pl.ion()
         pl.show()
-
 
     def PlotEdge(self, sconfig, econfig):
         pl.plot([sconfig[0], econfig[0]],
